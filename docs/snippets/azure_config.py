6e953e8cdd5a56a6a4f9a4f1b7aa943f30385422
Daniel Woste <daniel.woste@useblocks.com>
2022-05-09 13:37:06 +0200

    linter changes

--- docs/snippets/azure_config.py
+++ docs/snippets/azure_config.py
@@ -9,21 +9,21 @@ Item URL: `{{data.fields["System.TeamProject"]}}/{{data.id}} <https://dev.azure.
    {{data.fields["System.Description"]}}"""
 
 needs_services = {
-    'azure_config': {
-        'url': "https://dev.azure.com/useblocks",
-        'token': os.getenv('NEEDS_AZURE', ''),
-        'id_prefix': "AZURE_",
-        'query':  "[System.WorkItemType] = 'Issue'",
-        'content': azure_content,
-        'mappings': {
+    "azure_config": {
+        "url": "https://dev.azure.com/useblocks",
+        "token": os.getenv("NEEDS_AZURE", ""),
+        "id_prefix": "AZURE_",
+        "query": "[System.WorkItemType] = 'Issue'",
+        "content": azure_content,
+        "mappings": {
             "id": ["id"],
-            "type": 'spec',
+            "type": "spec",
             "title": ["fields", "System.Title"],
             "status": ["fields", "System.State"],
         },
-        'extra_data': {
-            "Original Type": ["fields", 'System.WorkItemType'],
-            "Original Assignee": ["fields", 'System.AssignedTo', 'displayName'],
-        }
+        "extra_data": {
+            "Original Type": ["fields", "System.WorkItemType"],
+            "Original Assignee": ["fields", "System.AssignedTo", "displayName"],
+        },
     }
 }
