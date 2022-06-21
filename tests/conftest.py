03421ddb0a8568b6be3152a173a657de72246ac9
Martin <martin.a.wieland@gmx.de>
2022-06-21 22:21:14 +0200

    CI test 12

--- tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from sphinx.testing.path import path
-
-pytest_plugins = "sphinx.testing.fixtures"
-
-
-@pytest.fixture(scope="session")
-def rootdir():
-    return path(__file__).parent.abspath() / "roots"
