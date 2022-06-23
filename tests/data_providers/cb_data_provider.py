26a277b9c055966b6c0252412f8aa870255f7ed0
Martin <martin.a.wieland@gmx.de>
2022-06-23 17:28:31 +0200

    initial structure

---
+++ tests/data_providers/cb_data_provider.py
@@ -0,0 +1,13 @@
+
+
+class cb_data_provider():
+
+    def __init__(self):
+        pass
+
+    def delete_all_projects(self):
+
+        # get projects
+        # use v1 to delete projects
+        
+        pass
